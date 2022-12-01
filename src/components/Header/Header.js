348b4003dac3dfdae2c1f96d039467a55461ef9d
MukeshSaren <mukeshsaren018@gmail.com>
2022-12-01 15:25:48 +0530

    complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,17 @@
 import Link from 'next/link';
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillFileText, AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons,Span } from './HeaderStyles';
 
 const Header = () => (
   <div>
     <Container>
       <Div1>
         <Link href="/">
-          <a style={{ display: "flex", alignment: "center", color: "white" }}>
-            <DiCssdeck size="3rem" /> <span>Portfolio</span>
+          <a style={{ display: "flex", alignment: "center", color: "white", marginBottom:"20;"}}>
+            <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
           </a>
         </Link>
       </Div1>
@@ -31,8 +31,18 @@ const Header = () => (
             <NavLink>About</NavLink>
           </Link>
         </li>
-        
       </Div2>
+      <Div3>
+        <SocialIcons href='https://www.github.com/MukeshSaren18'>
+          <AiFillGithub size="3rem"/>
+        </SocialIcons>
+        <SocialIcons href='https://www.linkedin.com'>
+          <AiFillLinkedin size="3rem"/>
+        </SocialIcons>
+        <SocialIcons href='https://www.github.com/MukeshSaren18'>
+          <AiFillFileText size="3rem"/>
+        </SocialIcons>
+      </Div3>
     </Container>
   </div>
 );
