90d55bdd88414c74d1d6e558c6a58e202f63b2b8
MukeshSaren <mukeshsaren018@gmail.com>
2022-12-03 00:37:44 +0530

    project commit

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,9 +5,31 @@ import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalCompon
 import { projects } from '../../constants/constants';
 
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id="projects">
+    <SectionDivider/>
+    <SectionTitle main > Projects </SectionTitle>
+    <GridContainer>
+      {[{
+        title: 'Project 1',
+        description: 'This is an project'
+      },{
+        title: 'Project 2',
+        description: 'This is an project'
+      },{
+        title: 'Project 3',
+        description: 'This is an project'
+      },{
+        title: 'Project 4',
+        description: 'This is an project'
+      }].map((project) => (
+        <div>
+          {project.title}
+          <br/>
+          {project.description}
+        </div>
+      ))}
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
